3d666859873b8df8e4f231be45d8a4c9ddcbc700
Intan Paoli <intankhairunnisa@gmail.com>
2021-01-20 15:27:39 +0100

    My first commit

---
+++ server/public/precache-manifest.a713b11f1fb44d17a891d1961a35d380.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5976063bff8a0003bc2b0d8d5d23083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bd6a3bde8afcf88b3be",
+    "url": "/static/css/main.ccb02f88.chunk.css"
+  },
+  {
+    "revision": "c2c84bee03c802f94314",
+    "url": "/static/js/2.2ad88393.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.2ad88393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd6a3bde8afcf88b3be",
+    "url": "/static/js/main.42ce6212.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
